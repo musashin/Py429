dee651055724a6daeeb46a3053c02855d27a521f
musashin <nulysse4@gmail.com>
2013-05-07 22:03:13 -0400

    Created unit test

---
+++ UnitTests/A429MsgFieldTest.py
@@ -0,0 +1,18 @@
+'''
+Created on 7 mai 2013
+
+@author: nulysse
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
