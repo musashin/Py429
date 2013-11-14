03ec117763b9abd883f984b02dc34d69e0fc3115
musashin <nulysse4@gmail.com>
2013-11-13 20:58:19 -0800

    Completed Testing for Parity Bit

--- ARINC429/UnitTests/A429ParityTest.py
+++ ARINC429/UnitTests/A429ParityTest.py
@@ -63,6 +63,24 @@ class testParity(unittest.TestCase):
         parityBitEven.setData(0x7FFFFFFF)
         self.assertEqual(parityBitEven.pack(),1<<31, "Parity Not Calculated Properly")
         
+    def testAFewCases(self):
+        '''
+        Further test the algorithm with a few samples cases manually generated
+        '''
+        cases = [   {'word':0b001100101,'parity':'even'},
+                    {'word':0b1110100100101,'parity':'odd'},
+                    {'word':0b10100101001010,'parity':'even'},
+                    {'word':0b10000010001,'parity':'odd'}]
+        
+        parityBitOdd = A429ParityBit.ParityBit('odd')
+        parityBitEven = A429ParityBit.ParityBit('even')
+        for case in cases:
+            parityBitOdd.setData(case['word'])
+            self.assertEqual(parityBitOdd.pack(),1<<31 if case['parity']=='even' else 0, "Parity Not Calculated Properly")
+            parityBitEven.setData(case['word'])
+            self.assertEqual(parityBitEven.pack(),0 if case['parity']=='even' else 1<<31, "Parity Not Calculated Properly")
+            
+        
     
         
         
